b3bdcf604fc751cd5e9dfe9b2912f757537e1fa0
niraj-wh <niraj@whitehatengineering.com>
2024-06-01 10:25:52 +0545

    upgrade to 2.4.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import { Select, SelectItem } from "@nextui-org/select";
+import React from "react";
 
 const presets = ["ironclad", "thunderstorm"];
 
 export default function Home() {
+  const [selectedPreset, setSelectedPreset] = React.useState(presets[0])
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <Select
         label="Compression preset:"
         className="w-[300px] flex-shrink-0 rounded-2xl"
         size="sm"
-        selectedKeys={["ironclad"]}
+        selectedKeys={[selectedPreset]}
         selectionMode="single"
         disallowEmptySelection
+        onChange={(evt) => setSelectedPreset(evt?.target?.value)}
       >
         {presets?.map((preset) => (
           <SelectItem
